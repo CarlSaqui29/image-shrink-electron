a4132ef06e809eb9e6d7f8f333b30cf7748f9728
Carl Saqui <carlsaqui000@gmail.com>
2022-12-02 08:03:22 +0800

    done

--- main.js
+++ main.js
@@ -1,110 +1,149 @@
-// destructuring
-// app -> manages the entire life cycle of application
-// BrowserWindow -> is a class used to create desktop windows
-// Menu is for upper menu of window
-// globalSHortcut is for 
-const { app, BrowserWindow, Menu, globalShortcut} = require('electron');
+const path = require('path')
+const os = require('os')
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron')
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash')
+const log = require('electron-log')
 
-// know what is the env (development, production) and know what OS
-// if packaging the app will set this to production
-process.env.NODE_ENV = 'development';
+// Set env
+process.env.NODE_ENV = 'production'
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false;
-const isMac = process.platform === 'darwin' ? true : false;
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
 
-let mainWindow;
-let aboutWindow;
+let mainWindow
+let aboutWindow
 
-const createMainWindow = () => {
-  // initializing desktop window
+function createMainWindow() {
   mainWindow = new BrowserWindow({
-    title: 'Image Shrink Application',
-    width: 500,
+    title: 'ImageShrink',
+    width: isDev ? 800 : 500,
     height: 600,
-    icon: './assets/icons/Icon_256x256.png'
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev ? true : false,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
   })
 
-  // load the mainWindow of your choice
-  mainWindow.loadFile('./src/index.html')
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  mainWindow.loadFile('./app/index.html')
 }
 
-const createAboutWindow = () => {
-  // initializing desktop window
+function createAboutWindow() {
   aboutWindow = new BrowserWindow({
-    title: 'Image Shrink Application',
-    width: 300 ,
+    title: 'About ImageShrink',
+    width: 300,
     height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     resizable: false,
-    icon: './assets/icons/Icon_256x256.png'
+    backgroundColor: 'white',
   })
 
-  // load the mainWindow of your choice
-  aboutWindow.loadFile('./src/about.html')
+  aboutWindow.loadFile('./app/about.html')
 }
 
-// display window
 app.on('ready', () => {
-  createMainWindow();
+  createMainWindow()
 
-  const mainMenu = Menu.buildFromTemplate(menu);
-  Menu.setApplicationMenu(mainMenu);
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
 
-  // for dev shorcuts
-  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
-  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools());
-
-  // garbage collection
-  mainWindow.on('closed', () => mainWindow = null);
-});
+  mainWindow.on('close', () => (mainWindow = null))
+})
 
 const menu = [
-  ...(isMac 
+  ...(isMac
     ? [
-    { 
-      label: app.name,
-      submenu: [ {
-        label: 'About',
-        click: createAboutWindow,
-        },  
-      ] 
-    }] : []),
-  ...(!isMac 
-    ? [
-    { 
-      label: 'Help',
-      submenu: [ {
-        label: 'About',
-        click: createAboutWindow,
-        },  
-      ] 
-    }] : []),
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
   {
     role: 'fileMenu',
   },
-  ...(isDev ? 
-  [
-    {
-      label: 'Developer',
-      submenu: [
-        { role: 'reload' },
-        { role: 'forcereload' },
-        { role: 'separator' },
-        { role: 'toggledevtools' },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
       ]
-    }
-  ]: [])
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
 ]
 
-// quit when all windows closed
+ipcMain.on('image:minimize', (e, options) => {
+  options.dest = path.join(os.homedir(), 'imageshrink')
+  shrinkImage(options)
+})
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    })
+
+    log.info(files)
+
+//     Changed from shell.openItem() for v9
+    shell.openPath(dest)
+
+    mainWindow.webContents.send('image:done')
+  } catch (err) {
+    log.error(err)
+  }
+}
+
 app.on('window-all-closed', () => {
-  // stay active until the user quits explicitly with Cmd + Q
-  if (isMac) { app.quit };
+  if (!isMac) {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
-  // create  a window in the app when the app on
-  // dock is click and there is no windows open
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow();
+    createMainWindow()
   }
 })
+
+app.allowRendererProcessReuse = true
